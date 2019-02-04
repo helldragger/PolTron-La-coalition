d83e4d87265d36bf7bb884ab7925d2435ab94756
Helldragger <christopher1223334444@live.fr>
2019-02-04 21:17:34 +0100

    Added Alexis Game module and a profiler to test its perfs

---
+++ poltron_game/Player.py
@@ -0,0 +1,56 @@
+from random import randint
+
+from poltron_game.TeamItems import *
+
+
+"""
+	author: Alexis Mortelier
+"""
+
+
+class Player(TeamItems):
+    speed = 1
+
+    def __init__(self, x, y, team):
+        TeamItems.__init__(self, x, y, team)
+        self.code = "*"
+        self.lastMove = ""
+
+    def __moveDown(self):
+        return (self.coord[0] + self.speed, self.coord[1])
+
+    def __moveUp(self):
+        return (self.coord[0] - self.speed, self.coord[1])
+
+    def __moveLeft(self):
+        return (self.coord[0], self.coord[1] - self.speed)
+
+    def __moveRight(self):
+        return (self.coord[0], self.coord[1] + self.speed)
+
+    def nextMove(self, direction):
+        if self.lastMove.upper() != "L" and direction.upper() == "D":
+            return self.__moveRight()
+        elif self.lastMove.upper() != "D" and direction.upper() == "Q":
+            return self.__moveLeft()
+        elif self.lastMove.upper() != "S" and direction.upper() == "Z":
+            return self.__moveUp()
+        elif self.lastMove.upper() != "Z" and direction.upper() == "S":
+            return self.__moveDown()
+        else:
+            return False
+
+    def nextMoveRandom(self):
+        num = randint(0, 3)
+        if num == 0:
+            return "D"
+        elif num == 1:
+            return "Q"
+        elif num == 2:
+            return "Z"
+        elif num == 3:
+            return "S"
+
+    def applyMove(self, direction):
+        self.lastMove = direction
+        self.coord = self.nextMove(direction)
